9cbe4aa60593e422fc0498e9473c663471c9a963
monanorremo <mona.norremo@gmail.com>
2021-03-01 21:19:49 +0000

    Add categories multiple choice dropdown

--- app.py
+++ app.py
@@ -106,11 +106,10 @@ def logout():
     return redirect(url_for("login"))
 
 
-
 @app.route("/add_recipe")
 def add_recipe():
-    return render_template("add_recipe.html")
-
+    categories = mongo.db.categories.find().sort("food_type", 1)
+    return render_template("add_recipe.html", categories=categories)
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
