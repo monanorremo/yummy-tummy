3e3170d6298544c8138e5d0fa85fba4d01b294c9
monanorremo <mona.norremo@gmail.com>
2021-03-04 21:12:32 +0000

    Add search function

--- app.py
+++ app.py
@@ -30,6 +30,12 @@ def get_recipes():
     recipes = list(mongo.db.recipes.find().sort("food_name", 1))
     return render_template("recipes.html", recipes=recipes)
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    recipes = list(mongo.db.recipes.find({"$text": {"$search": query}}))
+    return render_template("recipes.html", recipes=recipes)
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
